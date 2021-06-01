6bcdc1f95e4ad384c0456df3b6cce173ea688f76
Vlad Ponych <vladyslav.ponych@additiv.com>
2021-06-01 18:24:50 +0300

    Add styles and basic component

---
+++ src/Components/Title/Title.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+import './Title.css';
+
+export const Title = ({
+    type, children, className, tabIndex,
+}) => {
+    const H = `h${type}`;
+
+    const componentClasses = classNames(
+        'lib-title',
+        `lib-title-${type}`,
+        className,
+    );
+
+    return (
+        <H className={componentClasses} tabIndex={tabIndex}>
+            {children}
+        </H>
+    );
+};
+
+Title.propTypes = {
+    className: PropTypes.string,
+    type: PropTypes.oneOf([1, 2, 3, 4, 5, 6]),
+    children: PropTypes.node.isRequired,
+    tabIndex: PropTypes.number,
+};
+
+Title.defaultProps = {
+    className: '',
+    tabIndex: null,
+    type: 1,
+};
