012d5254249a1e4d129f44701269671f1c41199d
Stas <belinskyi@qarea.us>
2021-06-05 21:46:10 +0300

    add view of KAL-5 task

---
+++ src/Components/CheckboxFilterItem/CheckboxFilterItem.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import classNames from 'classnames';
+import './CheckboxFilterItem.css';
+
+const CheckboxFilterItem = (
+    className,
+) => {
+    const initialData = [
+        { name: 'Collection', id: 1 },
+        { name: 'Product Type', id: 2 },
+        { name: 'Price', id: 3 },
+        { name: 'Size', id: 4 },
+        { name: 'Colors', id: 5 },
+    ];
+
+    const [filterItem, setFilterItem] = useState(initialData);
+
+    const componentClasses = classNames(
+        'lib-checkboxFilterItem',
+        className,
+    );
+
+    return (
+        <>
+            <div className={componentClasses}>
+                <div className="filters__items">
+                    {filterItem.map((filterItem) => (
+                        <div className="filters__item" key={filterItem.id}>
+                            <span className="filters__text">{filterItem.name}</span>
+                            <span className="arrow" />
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default CheckboxFilterItem;
