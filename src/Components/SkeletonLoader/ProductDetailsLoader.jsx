b12b730fe90866ecc68b189cc50c85d11110e9de
Comratt <ponychvladyslav@gmail.com>
2022-06-03 14:43:09 +0300

    Fix product details loader

--- src/Components/SkeletonLoader/ProductDetailsLoader.jsx
+++ src/Components/SkeletonLoader/ProductDetailsLoader.jsx
@@ -1,27 +1,45 @@
 import React from 'react';
 import ContentLoader from 'react-content-loader';
-import { useDetectedMobileDevice } from '../../hooks/useDetectMobileDevice';
+import { useDetectedMobileDevice } from 'hooks/useDetectMobileDevice';
 
-const ProductDetailsLoader = ({ widthPadding = 0, ...props }) => {
-    const { clientWidth, isDesktopSize } = useDetectedMobileDevice();
-    const maxWidth = clientWidth > 1150 ? (1150 - widthPadding) : (clientWidth - widthPadding);
+const ProductDetailsLoader = (props) => {
+    const { clientWidth, isTabletSize } = useDetectedMobileDevice();
+    const maxWidth = clientWidth > 1400 ? (1400) : (clientWidth);
+    const imageWidth = clientWidth < 968 ? '40%' : `${clientWidth > 1400 ? '60%' : clientWidth / 2}`;
+
+    if (isTabletSize) {
+        return (
+            <ContentLoader viewBox={`0 0 ${maxWidth} 90vh`} height="90vh" width={maxWidth} {...props}>
+                <rect x="0" y="0" rx="0" ry="0" width={maxWidth} height="70vh" />
+                <rect x="30%" y="calc(70vh + 50px)" rx="2" ry="2" width="300" height="25" />
+                <rect x="34%" y="calc(70vh + 90px)" rx="2" ry="2" width="250" height="20" />
+                <rect x="38%" y="calc(70vh + 130px)" rx="2" ry="2" width="200" height="20" />
+            </ContentLoader>
+        );
+    }
 
     return (
-        <div style={{ float: 'right', marginRight: 50, maxWidth: 1150 }}>
-            <ContentLoader viewBox={`0 0 ${maxWidth} 600`} height={600} width={maxWidth} {...props}>
-                <circle cx="472" cy="159" r="7" />
-                <rect x="calc(100% - 320px)" y="70" rx="0" ry="0" width="250" height="25" />
-                <rect x="calc(100% - 320px)" y="110" rx="5" ry="5" width="60" height="20" />
-                <circle cx="calc(100% - 320px)" cy="170" rx="5" r="5" />
-                <rect x="calc(100% - 320px)" y="214" rx="5" ry="5" width="220" height="20" />
-                <rect x="calc(100% - 320px)" y="244" rx="5" ry="5" width="220" height="20" />
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <ContentLoader viewBox={`0 0 ${maxWidth} 800`} height={600} width={maxWidth} {...props}>
+                <rect x="calc(90% - 220px)" y="70" rx="0" ry="0" width="250" height="25" />
+                <rect x="calc(90% - 220px)" y="110" rx="5" ry="5" width="80" height="20" />
+                <circle cx="calc(90% - 200px)" cy="170" rx="15" r="15" />
+                <circle cx="calc(90% - 160px)" cy="170" rx="15" r="15" />
 
-                <rect x="160" y="30" rx="0" ry="0" width="50%" height="600" />
+                <rect x="calc(90% - 220px)" y="210" rx="5" ry="5" width="80" height="20" />
+                <rect x="calc(90% - 220px)" y="250" rx="7" ry="7" width="30" height="30" />
+                <rect x="calc(90% - 180px)" y="250" rx="7" ry="7" width="30" height="30" />
+                <rect x="calc(90% - 140px)" y="250" rx="7" ry="7" width="30" height="30" />
+                <rect x="calc(90% - 220px)" y="290" rx="5" ry="5" width="220" height="20" />
+                <rect x="calc(90% - 220px)" y="340" rx="5" ry="5" width="220" height="20" />
+                <rect x="calc(90% - 220px)" y="380" rx="5" ry="5" width="220" height="20" />
 
-                <rect x="10" y="30" rx="0" ry="0" width="50" height="60" />
-                <rect x="10" y="100" rx="0" ry="0" width="50" height="60" />
-                <rect x="10" y="170" rx="0" ry="0" width="50" height="60" />
-                <rect x="10" y="240" rx="0" ry="0" width="50" height="60" />
+                <rect x="160" y="40" rx="0" ry="0" width={imageWidth} height="600" />
+
+                <rect x="20" y="40" rx="0" ry="0" width="60" height="60" />
+                <rect x="20" y="110" rx="0" ry="0" width="60" height="60" />
+                <rect x="20" y="180" rx="0" ry="0" width="60" height="60" />
+                <rect x="20" y="250" rx="0" ry="0" width="60" height="60" />
             </ContentLoader>
         </div>
     );
