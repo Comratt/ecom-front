072857f6a533fd9ea5dd676af964f0a6c40f10b5
Comratt <ponychvladyslav@gmail.com>
2022-06-12 16:27:11 +0300

    Add home scroll

--- src/context/home/hooks/useHome.js
+++ src/context/home/hooks/useHome.js
@@ -1,5 +1,5 @@
 import {
-    useCallback, useEffect, useState, useMemo,
+    useCallback, useEffect, useState, useMemo, useRef,
 } from 'react';
 
 import { useLayout } from 'hooks/useLayout';
@@ -7,6 +7,7 @@ import { useHomeLayout } from './useHomeLayout';
 import { useHomeData } from './useHomeData';
 
 export const useHome = () => {
+    const scrollYRef = useRef(0);
     const [isTransparent, setTransparent] = useState(true);
     const {
         result,
@@ -17,6 +18,34 @@ export const useHome = () => {
         loadingCategories,
     } = useHomeData();
     const { changeTopNavState } = useLayout();
+
+    useEffect(() => {
+        const scrollYPosition = +localStorage.getItem('scrollYPositionHome');
+
+        if (!scrollYPosition) {
+            window.scrollTo(0, 0);
+        }
+
+        setTimeout(() => {
+            if (scrollYPosition) {
+                window.scrollTo(0, scrollYPosition);
+                localStorage.setItem('scrollYPositionHome', '0');
+            }
+        }, 700);
+    }, []);
+
+    useEffect(() => {
+        const handleScrollHome = () => {
+            scrollYRef.current = window.scrollY;
+        };
+
+        window.addEventListener('scroll', handleScrollHome);
+
+        return () => {
+            localStorage.setItem('scrollYPositionHome', scrollYRef.current);
+            window.removeEventListener('scroll', handleScrollHome);
+        };
+    }, []);
 
     const handleScroll = useCallback(
         () => setTransparent(!window.pageYOffset),
