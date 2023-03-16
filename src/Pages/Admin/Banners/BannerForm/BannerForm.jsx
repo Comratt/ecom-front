f530a1b432bd106882af0f11c9ca44e45a008367
Comratt <ponychvladyslav@gmail.com>
2023-03-16 20:41:09 +0200

    Add select to banners

--- src/Pages/Admin/Banners/BannerForm/BannerForm.jsx
+++ src/Pages/Admin/Banners/BannerForm/BannerForm.jsx
@@ -2,25 +2,60 @@ import React from 'react';
 import { getValidationMessage } from 'Constants';
 import Input from 'Components/Input';
 import { FIELDS, VALIDATION_RULES } from './constants';
+import { useFetchCategories } from '../../hooks/useFetchCategories';
 
-const BannerForm = ({ register, errors }) => (
-    <>
-        {FIELDS.map((field) => (
-            <div className="form-row mb-2" key={field.name}>
-                <div className="col-12">
-                    <Input
-                        ref={register(VALIDATION_RULES[field.name])}
-                        name={field.name}
-                        type={field.type}
-                        required={field.required}
-                        placeholder={field.placeholder}
-                        label={field.label}
-                        error={getValidationMessage(errors[field.name])}
-                    />
-                </div>
-            </div>
-        ))}
-    </>
-);
+const BannerForm = ({ register, errors, link }) => {
+    const {
+        loading, error, categories, setCategories,
+    } = useFetchCategories();
+
+    return (
+        <>
+            {FIELDS.map((field) => {
+                if (field.type === 'select') {
+                    return (
+                        <div className="form-row mb-2" key={field.name}>
+                            <div className="col-12">
+                                <label className="my-1 mr-2" htmlFor="inlineFormCustomSelectPref">{field.label}</label>
+                                <select
+                                    name={field.name}
+                                    ref={register(VALIDATION_RULES[field.name])}
+                                    className="custom-select my-1 mr-sm-2"
+                                    id="inlineFormCustomSelectPref"
+                                >
+                                    <option value={0} disabled>Категория</option>
+                                    {categories.map((category) => (
+                                        <option
+                                            value={`/collection/${category.category_id}`}
+                                            selected={`/collection/${category.category_id}` === link}
+                                        >
+                                            {category.name}
+                                        </option>
+                                    ))}
+                                </select>
+                            </div>
+                        </div>
+                    );
+                }
+
+                return (
+                    <div className="form-row mb-2" key={field.name}>
+                        <div className="col-12">
+                            <Input
+                                ref={register(VALIDATION_RULES[field.name])}
+                                name={field.name}
+                                type={field.type}
+                                required={field.required}
+                                placeholder={field.placeholder}
+                                label={field.label}
+                                error={getValidationMessage(errors[field.name])}
+                            />
+                        </div>
+                    </div>
+                );
+            })}
+        </>
+    );
+};
 
 export default BannerForm;
