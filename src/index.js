7c4d507dcdddb8c272ed28e349e739f5ded5ea39
Mr-Oleksandr <skovalov777@gmail.com>
2023-04-25 19:30:08 +0300

    add module for gtm

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import TagManager from 'react-gtm-module';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
+const tagManagerArgs = {
+    gtmId: 'GTM-K6QQZCH',
+};
+
+TagManager.initialize(tagManagerArgs);
 
 ReactDOM.render(
     // eslint-disable-next-line
